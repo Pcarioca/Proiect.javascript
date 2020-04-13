7171c8cac97c3b067bcab0c23511a9ebd93816f5
Andrei Munteanu <incognitopaul97@gmail.com>
2020-04-13 07:32:31 +0300

    uuu
    ggg

---
+++ index.js
@@ -0,0 +1,30 @@
+var i=0;
+var todo=" ";
+function add(){
+    i++;
+    var hatz={todo, todo2, todo3,todo4, todo5, todo6, todo7, todo8, todo9, todo10,todo11, todo12, todo13, todo14, todo15, todo16, fictiva=" "};
+    todo=document.getElementById("input").value;
+    for(x in person){
+        person[x+1]=person[x];
+    }
+    
+
+
+
+    document.getElementById("todo").innerHTML = todo;
+    document.getElementById("todo2").innerHTML = todo2;
+    document.getElementById("todo3").innerHTML = todo3;
+    document.getElementById("todo4").innerHTML = todo4;
+    document.getElementById("todo5").innerHTML = todo5;
+    document.getElementById("todo6").innerHTML = todo6;
+    document.getElementById("todo7").innerHTML = todo7;
+    document.getElementById("todo8").innerHTML = todo8;
+    document.getElementById("todo9").innerHTML = todo9;
+    document.getElementById("todo10").innerHTML = todo10;
+    document.getElementById("todo11").innerHTML = todo11;
+    document.getElementById("todo12").innerHTML = todo12;
+    document.getElementById("todo13").innerHTML = todo13;
+    document.getElementById("todo14").innerHTML = todo14;
+    document.getElementById("todo15").innerHTML = todo15;
+    
+}
